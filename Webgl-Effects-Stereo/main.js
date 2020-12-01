cf080be0d446b505884d3307a74c3170e4f0346b
Choi Jiwon <winning1993@naver.com>
2020-12-01 13:15:31 +0900

    [ADD] 프로젝트 초기화

---
+++ Webgl-Effects-Stereo/main.js
@@ -0,0 +1,11 @@
+let container, camera, scene, renderer, effect
+
+const spheres = []
+
+let mouseX = 0
+let mouseY = 0
+
+let windowHalfX = window.innerWidth / 2
+let windowHalfY = window.innerHeight / 2
+
+function init() {}
